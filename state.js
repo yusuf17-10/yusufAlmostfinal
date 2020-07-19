0cdcc26170f1f303f00f3a8143a6fb1f72238da6
yusuf17-10 <64640684+yusuf17-10@users.noreply.github.com>
2020-07-19 20:04:27 +0530

    Add files via upload

---
+++ state.js
@@ -0,0 +1,48 @@
+class Game{
+    constructor(){
+
+    }
+
+    getState(){
+        var gameStateRef  = database.ref('gameState');
+        gameStateRef.on("value",function(data){
+           gameState = data.val();
+        })
+       
+      }
+    
+      update(state){
+        database.ref('/').update({
+          gameState: state
+        });
+      }
+    
+      start(){
+        if(gameState === 0){
+          user = new User();
+          user.getCount();
+          User.getUserInfo();
+          form = new Form();
+          form.display();
+          timetable=new TimeTable();
+          timetable.hide();
+          homepage=new HomePage();
+          homepage.hide();
+        }
+      }
+
+      signup(){
+        //if(gameState === 1){
+          
+          form.hide();
+          homepage.hide();
+          timetable.display();
+     // }
+    }
+      home(){
+          User.getUserInfo();
+          timetable.hide();
+          homepage.display();
+       // }
+      }
+}
